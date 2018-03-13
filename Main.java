d8caf6c37e76d2fdff459885f4c80823357fccc5
105306008 <31977474+105306008@users.noreply.github.com>
2018-03-13 13:09:40 +0800

    Update Main

---
+++ Main.java
@@ -0,0 +1,62 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+
+		Scanner in = new Scanner(System.in);
+
+
+		String name = in.next();
+		double weight = 1;
+		Keyword k = new Keyword(name, weight);
+		String keyword = k.name;
+
+//		String keyword = in.next();
+			
+        KeywordList keywordList = new KeywordList();
+		ArrayList<Keyword> keywords = new ArrayList<Keyword>();
+		keywords = keywordList.keywords;
+		keywords.add(k);
+		
+		Query gq = new Query(keyword);
+		ArrayList<WebTree> trees = new ArrayList<WebTree>();
+		gq.query();
+		gq.find();
+		trees = gq.trees;
+		
+		HeapSort treeSort = new HeapSort();
+		
+//		System.out.println("test");
+		for(int i=0; i<trees.size(); i++) {
+			try {
+		  trees.get(i).setPostOrderScore(keywords);
+		  treeSort.add(trees.get(i));
+		  }catch(Exception e) {
+			  
+		  }
+		}
+		
+		for(int i=0; i<trees.size(); i++) {
+			try {
+		  WebTree tree = (WebTree) treeSort.removeMin();
+			//System.out.print(i+1, tree.startNode.webpage.name)
+		  System.out.println("\n\n" + (i+1) + " " + tree.root.webPage.toString() + "," + tree.root.nodeScore);
+		  tree.eularPrintTree();
+		  System.out.println("\n");
+			}catch(Exception e) {
+				continue;
+			}
+		}
+
+		
+		
+		
+	}
+
+}
